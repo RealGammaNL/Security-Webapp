101da3099916edc8ab32061b5e981020b1ecd53f
RealGammaNL <max@brosky.net>
2024-04-08 11:04:27 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class SecurityDb
+{
+	public SecurityDb()
+	{
+		// DbContext constructor      
+        public SecurityDb(DbContextOptions<SecurityDb> contextOptions) 
+            : base(contextOptions)
+            {
+            
+            }
+
+        // DbContext OnConfiguring method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        //DbContext OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+	}
+}
---
+++ Security Webapp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Security_Webapp.Data;
+using Security_Webapp.Services;
+using Security_Webapp.Models;
+using System.Diagnostics;
+
+namespace Security_Webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
